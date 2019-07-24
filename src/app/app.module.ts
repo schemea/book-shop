df7177d6b4f3e9b72ff704c48d45b0e0435d0e41
Ida55 <50695161+Ida55@users.noreply.github.com>
2019-07-24 12:55:48 +0200

    Add cart component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { BookCollectionComponent } from "./book/collection/book-collection.compo
 import { BookDetailsComponent } from "./book/details/book-details.component";
 import { SearchComponent } from "./routing/search/search.component";
 import { DetailsComponent } from './routing/details/details.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { DetailsComponent } from './routing/details/details.component';
     BookCollectionComponent,
     BookDetailsComponent,
     SearchComponent,
-    DetailsComponent
+    DetailsComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
