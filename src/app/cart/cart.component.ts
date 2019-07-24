4f91c5a3bb7abe60d772b9a3bcbd5088bd3a6425
Stéphane CHEA <19475666+Eriphid@users.noreply.github.com>
2019-07-24 16:55:12 +0200

    fix merge error

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,18 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { CartService } from "app/services/cart";
 
 @Component({
-  selector: 'app-cart',
-  templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  selector: "app-cart",
+  templateUrl: "./cart.component.html",
+  styleUrls: ["./cart.component.scss"]
 })
 export class CartComponent implements OnInit {
 
-  
 
-  constructor(private cart:CartService) { 
-    ()
-  }
+
+  constructor(private cart: CartService) {   }
 
   ngOnInit() {
   }
