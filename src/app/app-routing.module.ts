a65dab0868897f0fcc24bbe23ea19a5c0670d0cb
Ida55 <50695161+Ida55@users.noreply.github.com>
2019-07-24 17:21:03 +0200

    updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { SearchComponent } from "./routing/search/search.component";
 import { DetailsComponent } from "./routing/details/details.component";
+import { CartComponent } from "./cart/cart.component";
 
 const routes: Routes = [
   {path: "book/:id", component: DetailsComponent},
-  {path: "", component: SearchComponent}
+  {path: "", component: SearchComponent},
+  {path: "cart", component: CartComponent }
 ];
 
 @NgModule({
